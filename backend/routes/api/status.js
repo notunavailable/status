7c8c9fb7088b53dc2054e88dc92e6bdafd4fc40e
notunavailable <leafnosebat@gmail.com>
2021-12-13 22:07:52 -0800

    added more apis

--- backend/routes/api/status.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const Status = require('../../models/Status.js');
-const router = express.Router();
-var ObjectId = require('mongodb').ObjectID;
-
-module.exports = router;
